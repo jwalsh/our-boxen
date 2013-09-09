d09bc3f6ed77eccf1b715ddaf6eaf27863f6b42a
Jason Walsh <jwalsh@bluekai.com>
2013-09-09 11:10:57 -0700

    Simple Emacs setup

--- modules/people/manifests/jwalsh.pp
+++ modules/people/manifests/jwalsh.pp
@@ -1,17 +1,6 @@
 class people::jwalsh {
-  include emacs   # requires emacs module in Puppetfile
-  include sparrow # requires sparrow module in Puppetfile
+  include projects:all
 
-  $home     = "/Users/${::boxen_user}"
-  $my       = "${home}/sandbox"
-  $dotfiles = "${my}/dotfiles"
-
-  file { $my:
-    ensure  => directory
-  }
-
-  repository { $dotfiles:
-    source  => 'jwalsh/dotfiles',
-    require => File[$my]
-  }
+  include emacs
+  include iterm2::stable
 }
