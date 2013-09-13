e56a2dea81fc203cd66f5dcd38e738c3946e0de1
Jason Walsh <jwalsh@bluekai.com>
2013-09-13 14:59:00 -0700

    Validating node install for coffeelint

--- modules/projects/manifests/work.pp
+++ modules/projects/manifests/work.pp
@@ -3,6 +3,8 @@ class projects::work {
     include qt
     include pow
     include mysql
+    include chruby
+    include nodejs::v0_10
 
   boxen::project { 'work':
     dotenv        => true,
@@ -10,4 +12,14 @@ class projects::work {
     nginx         => true,
     ruby          => '1.9.3'
   }
+
+  # nodejs::module { ['coffee-script', 'meteorite']:
+  #   node_version => 'v0.10'
+  # }
+
 }
+
+# class { 'nodejs::global':
+#   version => 'v0.10'
+# }
+
