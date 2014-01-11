d14a3f3ec1d8408a81fb8450dbb2dd3799577704
Jason Walsh <github@jwalsh.net>
2014-01-10 18:33:06 -0800

    Work

---
+++ modules/projects/manifests/work.pp
@@ -0,0 +1,17 @@
+class projects::work {
+
+    include clojure
+
+    boxen::project { 'work':
+      mysql         => true,
+      nginx         => true,
+      source        => 'jwalsh/cloaked-happiness-robo'
+    }
+
+    nodejs::module { ['coffee-script', 'meteorite']:
+      node_version => 'v0.10'
+    }
+
+  }
+
+
